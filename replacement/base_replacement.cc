fddabe21f17dca997c525908b82acb5cf6907c30
skhan-org <mailtoshahnawazk@gmail.com>
2022-05-07 22:52:56 +0530

    Updated FindVictim

--- replacement/base_replacement.cc
+++ replacement/base_replacement.cc
@@ -15,15 +15,13 @@ void CACHE::update_replacement_state(uint32_t cpu, uint32_t set, uint32_t way, u
 
     return lru_update(set, way);
 }
-
 uint32_t CACHE::lru_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
 {
     uint32_t way = 0;
-
     // fill invalid line first
     for (way=0; way<NUM_WAY; way++) {
         if (block[set][way].valid == false) {
-
+        
             DP ( if (warmup_complete[cpu]) {
             cout << "[" << NAME << "] " << __func__ << " instr_id: " << instr_id << " invalid set: " << set << " way: " << way;
             cout << hex << " address: " << (full_addr>>LOG2_BLOCK_SIZE) << " victim address: " << block[set][way].address << " data: " << block[set][way].data;
@@ -62,6 +60,7 @@ void CACHE::lru_update(uint32_t set, uint32_t way)
     for (uint32_t i=0; i<NUM_WAY; i++) {
         if (block[set][i].lru < block[set][way].lru) {
             block[set][i].lru++;
+            
         }
     }
     block[set][way].lru = 0; // promote to the MRU position
--- src/uncore.cc
+++ src/uncore.cc
@@ -5,5 +5,8 @@ UNCORE uncore;
 
 // constructor
 UNCORE::UNCORE() {
-
+    for (int i=0;i<NUM_CPUS;i++)
+    {
+        wayAllocated[i]=LLC_WAY/NUM_CPUS;
+    }
 }
