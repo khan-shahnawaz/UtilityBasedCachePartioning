0ed05aad853b177d91fc52d17d7bd766172c810f
skhan-org <mailtoshahnawazk@gmail.com>
2022-05-10 10:14:01 +0530

    Completed ATD and UMON counters

--- src/AuxiliaryTagDirectory.cc
+++ src/AuxiliaryTagDirectory.cc
@@ -2,19 +2,62 @@
 #include "cache.h"
 #include "uncore.h"
 #include "ooo_cpu.h"
-int UpdateATD(PACKET *packet)
+void UpdateATD(PACKET *packet)
 {
     int set= uncore.LLC.get_set(packet->address);
-    if (set%SamplingFrequency==0)
+    if (set%SamplingFrequency==0) //If the set is included in Set sampling
     {
-        ATD *myTagDirectory = &ooo_cpu[packet->cpu].TagDirectory;
-        int mySet = set/SamplingFrequency;
-        for (int i=0;i<LLC_WAY;i++)
+        ATD *myTagDirectory = &ooo_cpu[packet->cpu].TagDirectory; // Getting tag directory of the CPU sending a packet
+        int mySet = set/SamplingFrequency; //The set in the ATD is mapped to set in the LLC
+        for (int i=0;i<LLC_WAY;i++) //Checking if it is a hit in ATD
         {
             if (myTagDirectory->ATDBlock[mySet][i].valid==1 && myTagDirectory->ATDBlock[mySet][i].tag==packet->address)
             {
-                
+                myTagDirectory->UMON_Global[myTagDirectory->ATDBlock[mySet][i].lru]++; //Incrementing the corresponding LRU position in the UMON after a hit
+                for (int way=0;way<LLC_WAY;way++) // Updating the LRU position of the ATD Block
+                {
+                    if (myTagDirectory->ATDBlock[mySet][i].lru>myTagDirectory->ATDBlock[mySet][way].lru)
+                    {
+                        myTagDirectory->ATDBlock[mySet][way].lru++;
+                    }
+                }
+                myTagDirectory->ATDBlock[mySet][i].lru=0;
+                return;
             }
+        }
+        for (int i=0;i<LLC_WAY;i++)
+        {
+            if (myTagDirectory->ATDBlock[mySet][i].valid==0)
+            {
+                myTagDirectory->ATDBlock[mySet][i].tag=packet->address;
+                myTagDirectory->ATDBlock[mySet][i].valid=1;
+                for (int way=0;way<LLC_WAY;way++)
+                {
+                    if (myTagDirectory->ATDBlock[mySet][way].lru<myTagDirectory->ATDBlock[mySet][i].lru)
+                    {
+                        myTagDirectory->ATDBlock[mySet][way].lru++;
+                    }
+                }
+                myTagDirectory->ATDBlock[mySet][i].lru=0;
+                return;
+            }
+        }
+        for (int i=0;i<LLC_WAY;i++)
+        {
+            if (myTagDirectory->ATDBlock[mySet][i].lru==LLC_WAY-1)
+            {
+                myTagDirectory->ATDBlock[mySet][i].tag=packet->address;
+                for (int way=0;way<LLC_WAY;way++)
+                {
+                    if (myTagDirectory->ATDBlock[mySet][way].lru<myTagDirectory->ATDBlock[mySet][i].lru)
+                    {
+                        myTagDirectory->ATDBlock[mySet][way].lru++;
+                    }
+                }
+                myTagDirectory->ATDBlock[mySet][i].lru=0;
+                return;
+            }
+            
         }
     }
 }
